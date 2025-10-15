743d197cd4e78ac677bd9fefc7a2287bdaeea9d9
Soo Qirun <sooqirun99@gmail.com>
2025-10-15 17:16:34 +0800

    server 4

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.DEV
-  ? "http://localhost:5919/api"
-  : "http://b15-qirun.mak3r.dev/api";
+  ? "http://localhost:5919/api/"
+  : "http://b15-qirun.mak3r.dev/api/";
